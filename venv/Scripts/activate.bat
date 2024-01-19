d4b14ee72039385d1adab8499310c36d8d0cf2a2
bademos <curbademus@yandex.ru>
2024-01-19 18:53:00 +0300

    third

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\user\py_projects\proj_1\venv
+set VIRTUAL_ENV=C:\Users\vadique\python_projects\proj_1\vadique_bot\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
